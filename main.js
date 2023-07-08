bb0a28f808d12d46722c3906db58aaa3b347d370
AnshTheCode <100848942+AnshTheCode@users.noreply.github.com>
2023-07-08 13:47:56 +0530

    hello my friends
    Wanna some tea-chai!

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-song = "";
+Peter_pan_song="";
+Harry_potter_theme_song="";
 
-function preload()
-{
-    
-}
-
-
-function setup()
-{
-    canvas = createCanvas(600, 400);
+function setup(){
+    canvas = createCanvas(600,530);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
 }
 
-function draw()
-{
-    image(video, 0, 0, 600, 500);
+function preload(){
+    Peter_pan_song = loadSound("music2.mp3");
+    Harry_potter_theme_song = loadSound("music.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,530);
 }
